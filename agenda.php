706cfa7f34487a177edd43cb26c83aef77e8ac96
Alexandre Magno <alexanmtz@gmail.com>
2012-06-05 00:02:58 -0300

    agenda file to use with the plugin

---
+++ agenda.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<!--  main content -->
+<div class="main-content grid_16">
+  <div class="grid_12">
+	<h2>Eventos</h2>
+	<div id="wp-agenda-calendar"></div>
+  </div>
+</div>
+<?php get_footer(); ?>
